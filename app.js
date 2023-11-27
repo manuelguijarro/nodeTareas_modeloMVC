0b699b5e9a4a0681e58a7a9a188db9b30debb6b0
manuelguijarro <manu2506.1994@gmail.com>
2023-11-28 00:06:33 +0100

    initProject

---
+++ app.js
@@ -0,0 +1,10 @@
+//Importamos los modulos que vamos a utilizar en este proyecto
+import express from 'express';
+import path from 'path';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+
+
+import express from 'express';
